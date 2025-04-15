ce889b62e3ccebcd72668b5c0d4146ec1ef2c7e4
Swad-Ski-Racing <blancclothes1@gmail.com>
2025-04-15 16:26:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,17 @@ function checkVisibility() {
       target.scrollIntoView({ behavior: 'smooth' });
     }
   });    
+  
+  toTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+});
